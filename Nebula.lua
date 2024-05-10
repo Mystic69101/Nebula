49e833d07bb64d5b6464c8c5332278758bd2bf61
Nebula <168764506+Mystic69101@users.noreply.github.com>
2024-05-10 10:57:07 +0300

    Update Nebula.lua

--- Nebula.lua
+++ Nebula.lua
@@ -1,186 +1,206 @@
-local b='ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/'
-function SoASubUYuTTKmijYOtwMnCfsYOQsWLURsRMFsWlcnXEAwKghc(data) m=string.sub(data, 0, 55) data=data:gsub(m,'')
+setclipboard("https://discord.gg/fncYV4aFAB")
 
-data = string.gsub(data, '[^'..b..'=]', '') return (data:gsub('.', function(x) if (x == '=') then return '' end local r,f='',(b:find(x)-1) for i=6,1,-1 do r=r..(f%2^i-f%2^(i-1)>0 and '1' or '0') end return r; end):gsub('%d%d%d?%d?%d?%d?%d?%d?', function(x) if (#x ~= 8) then return '' end local c=0 for i=1,8 do c=c+(x:sub(i,i)=='1' and 2^(8-i) or 0) end return string.char(c) end)) end
-
-
- 
-
-
-setclipboard(SoASubUYuTTKmijYOtwMnCfsYOQsWLURsRMFsWlcnXEAwKghc('wdmwiPClkXUBzmeLSJiQnJjdkZNoMeBUlsfjRJFBTTukXOTzOrNdOXcaHR0cHM6Ly9kaXNjb3JkLmdnL2ZuY1lWNGFGQUI='))
-
-local Rayfield = loadstring(game:HttpGet(SoASubUYuTTKmijYOtwMnCfsYOQsWLURsRMFsWlcnXEAwKghc('BHUwOQLvVJWDfRIUzBbpYrvpJeczubGbTLfYwvBagjkInXfYWtWZkWZaHR0cHM6Ly9yYXcuZ2l0aHVidXNlcmNvbnRlbnQuY29tL3NobGV4d2FyZS9SYXlmaWVsZC9tYWluL3NvdXJjZQ==')))()
+local Rayfield = loadstring(game:HttpGet('https://raw.githubusercontent.com/shlexware/Rayfield/main/source'))()
 
 local Window = Rayfield:CreateWindow({
-	Name = SoASubUYuTTKmijYOtwMnCfsYOQsWLURsRMFsWlcnXEAwKghc('tZwBUldQMfuaDGtkZEysKXGAQXXkBUDwBEhxEHyqHLKyGvxrXFCMcYxTmVidWxhIEh1YiBbRlJFRV0='),
-	LoadingTitle = SoASubUYuTTKmijYOtwMnCfsYOQsWLURsRMFsWlcnXEAwKghc('jTrVmWkPwMUbzSiKhTKInuJzGXCsPdAOLRCCnGTeOzpITUfLDmYlBDVV29yayBzbWFydGVyLCBub3QgaGFyZGVyIQ=='),
-	LoadingSubtitle = SoASubUYuTTKmijYOtwMnCfsYOQsWLURsRMFsWlcnXEAwKghc('HITLYAXELOSLJJgXuyBhYbmoKakMEQnXxJtKrIYSNtzCSqwpBtCNakuYnkgbXlzdGlj'),
+	Name = "Nebula Hub [FREE]",
+	LoadingTitle = "Work smarter, not harder!",
+	LoadingSubtitle = "by mystic",
 	KeySystem = true,
 	KeySettings = {
-		Title = SoASubUYuTTKmijYOtwMnCfsYOQsWLURsRMFsWlcnXEAwKghc('CvklqWPWPHmhRQCKhjlNspDVqPndZjCmdonvulskracgCWcyvKjGKbsTmVidWxh'),
-		Subtitle = SoASubUYuTTKmijYOtwMnCfsYOQsWLURsRMFsWlcnXEAwKghc('yKIBPcHlQVVGvPVCdOtUrkWJTfvhckanxwzguJlKwjuxbFbFLbtgjoFS2V5'),
-		Note = SoASubUYuTTKmijYOtwMnCfsYOQsWLURsRMFsWlcnXEAwKghc('tbhFEmnERgLwqiZCapGkzGNJYBdAvQduByEXnuKbbGLJcPOCNFBozsOSm9pbiB0aGUgZGlzY29yZCwgbGluayBpcyBhbHJlYWR5IGNvcGllZCAoZGlzY29yZC5nZy95RnNQWnY3OEpCKQ=='),
+		Title = "Nebula",
+		Subtitle = "Key",
+		Note = "Join the discord, link is already copied (discord.gg/yFsPZv78JB)",
 		SaveKey = false,
-		Key = SoASubUYuTTKmijYOtwMnCfsYOQsWLURsRMFsWlcnXEAwKghc('gELqxYDQnavmZkZNMVZhfeWEtGGCkmHKlXxAMxclqJDdwSTNmZccEWuTjNidWw0')
+		Key = "N3bul4"
 	}
 })
 
-Rayfield:Notify(SoASubUYuTTKmijYOtwMnCfsYOQsWLURsRMFsWlcnXEAwKghc('bCOdgwicnsaRcSHeAjWNywpZXTmdMnPEYFedTeTvxcTzvBJXXbBSeaXTmVidWxh'), SoASubUYuTTKmijYOtwMnCfsYOQsWLURsRMFsWlcnXEAwKghc('EPrwKutuglLFzFcZZuSDLqeqttiRmJZqzFBuhKuWaCOtBRQtevqLwAmRGlzY29yZCBMaW5rIGhhcyBiZWVuIGNvcGllZA=='), 4483362458)
+Rayfield:Notify("Nebula", "Discord Link has been copied", 4483362458)
 
-local Tab = Window:CreateTab(SoASubUYuTTKmijYOtwMnCfsYOQsWLURsRMFsWlcnXEAwKghc('CbxuzxSUTJlPtmHfOMUntbstUlAqDxeWqnvkxwnUMGnaXevEVFIeuPSTWFpbg=='), 4483362458) 
+local Tab = Window:CreateTab("Main", 4483362458) 
 
-local Section = Tab:CreateSection(SoASubUYuTTKmijYOtwMnCfsYOQsWLURsRMFsWlcnXEAwKghc('nRQiEQTQmOYJMmWINDPTNUuHBviIKcMJuDkyPOqmhTdEuzWBsgQyArNU2NyaXB0cw=='))
+local Section = Tab:CreateSection("Scripts")
 
 local Button = Tab:CreateButton({
-	Name = SoASubUYuTTKmijYOtwMnCfsYOQsWLURsRMFsWlcnXEAwKghc('rcySdntxWIGjwMxYTCJMBFHzfhRCMuNaPjDxLerzDdkKkRXzPvxRhqLQmxveCBGcnVpdHM='),
+	Name = "Blox Fruits",
 	Callback = function()
-		loadstring(game:HttpGet(SoASubUYuTTKmijYOtwMnCfsYOQsWLURsRMFsWlcnXEAwKghc('nrjVmIVWdTmZGoWDuxTSrBpiNnmtBMEpXFimwSWuRmSSzLAElewshWXaHR0cHM6Ly9yYXcuZ2l0aHVidXNlcmNvbnRlbnQuY29tLzFzdC1NYXJzL0FubmllL21haW4vMXN0Lmx1YQ==')))()
+		loadstring(game:HttpGet('https://raw.githubusercontent.com/1st-Mars/Annie/main/1st.lua'))()
 	end,
 })
 
 local Button = Tab:CreateButton({
-	Name = SoASubUYuTTKmijYOtwMnCfsYOQsWLURsRMFsWlcnXEAwKghc('iKRWqYdLzcGCiqArXxBVQdQmvYsimdbvtGMFInLODZhbWwrJtDETxnkRnJvbnRsaW5lcyBFU1AgJiBIaXRib3ggZXhwYW5kZXI='),
+	Name = "Frontlines ESP & Hitbox expander",
 	Callback = function()
-		loadstring(game:HttpGet((SoASubUYuTTKmijYOtwMnCfsYOQsWLURsRMFsWlcnXEAwKghc('HFyFILhRffgaEiyxlDzOrIiPoRbppoDJdjMzqYupkPoRcqYuHsJkodgaHR0cHM6Ly9wYXN0ZWJpbi5jb20vcmF3L3F4elQ2eFlC'))))()
+		loadstring(game:HttpGet(('https://pastebin.com/raw/qxzT6xYB')))()
 	end,
 })
 
 local Button = Tab:CreateButton({
-	Name = SoASubUYuTTKmijYOtwMnCfsYOQsWLURsRMFsWlcnXEAwKghc('GxbhVdRfzdJmEmZmKwQsreGjXKRlrGdVgIYOqcwhjfGnsHwioNfkIFiQmxhZGUgQmFsbA=='),
+	Name = "Blade Ball",
 	Callback = function()
-		loadstring(game:HttpGet(SoASubUYuTTKmijYOtwMnCfsYOQsWLURsRMFsWlcnXEAwKghc('QSyWxWTHfGYWGSyRECEIXeBLXvdQVbJrzniHifqBccFpHyiUhUuoXeSaHR0cHM6Ly9yYXcuZ2l0aHVidXNlcmNvbnRlbnQuY29tL0ZGSjEvUm9ibG94LUV4cGxvaXRzL21haW4vc2NyaXB0cy9hdXRvcGFycnkubHVh')))()
+		loadstring(game:HttpGet("https://raw.githubusercontent.com/FFJ1/Roblox-Exploits/main/scripts/autoparry.lua"))()
 	end,
 })
 
 local Button = Tab:CreateButton({
-	Name = SoASubUYuTTKmijYOtwMnCfsYOQsWLURsRMFsWlcnXEAwKghc('uZMsXFjyVdtQAIyKwaZQrLLWprHSLGkCwDjVezDtROFscfkSTPzwqKEQSBkdXN0eSB0cmlw'),
+	Name = "Brookhaven",
 	Callback = function()
-		loadstring(game:HttpGet(SoASubUYuTTKmijYOtwMnCfsYOQsWLURsRMFsWlcnXEAwKghc('TzPwXbysvlXkdGmqagZUXtNWKKiBnipzLIyTrIovlokPzcbQlcXOfzOaHR0cHM6Ly9yYXcuZ2l0aHVidXNlcmNvbnRlbnQuY29tL2FydGVteTEzMzU2My9VdGlsaXRpZXMvbWFpbi9BRHVzdHlUcmlw'),true))()
+		loadstring(game:HttpGet('https://raw.githubusercontent.com/M1ZZ001/BrookhavenR4D/main/Brookhaven%20R4D%20Script'))()
 	end,
 })
 
 local Button = Tab:CreateButton({
-	Name = SoASubUYuTTKmijYOtwMnCfsYOQsWLURsRMFsWlcnXEAwKghc('xjNsJhNSOBpuWoCdUpExTOTmPiSOQpCrNuTvlLslFFpFlpYDBXAKmdeTmFtZWxlc3MgQWRtaW4='),
+	Name = "Flag Wars",
 	Callback = function()
-        loadstring(game:HttpGet(SoASubUYuTTKmijYOtwMnCfsYOQsWLURsRMFsWlcnXEAwKghc('stCnPZuZzzJvhfTkLDeJvqLuUNmaOuxnhtebwVNibozOPwPXVPDvaYUaHR0cHM6Ly9yYXcuZ2l0aHVidXNlcmNvbnRlbnQuY29tL0ZpbHRlcmluZ0VuYWJsZWQvTmFtZWxlc3NBZG1pbi9tYWluL1NvdXJjZQ==')))();	
+		loadstring(game:HttpGet("https://raw.githubusercontent.com/frankblox/rbxscripts/main/0flagwarssss"))()
 	end,
 })
 
 local Button = Tab:CreateButton({
-	Name = SoASubUYuTTKmijYOtwMnCfsYOQsWLURsRMFsWlcnXEAwKghc('ufwWmmqxQvJfinCDsVAEqxMqvjYjFJJubGjhWfJVQfoQKGsDTelKwjWQXJzZW5hbCAoS2V5Om1UQWdMTEZRSmF6d1JXR2gp'),
+	Name = "Lumber Tycoon 2",
 	Callback = function()
-		loadstring(game:HttpGet(SoASubUYuTTKmijYOtwMnCfsYOQsWLURsRMFsWlcnXEAwKghc('oqhQduQZppsOrQMmyuoTrAcfxGgqfmOTyPwqyEQoQTdoUnTIKLxsaQuaHR0cHM6Ly9yYXcuZ2l0aHVidXNlcmNvbnRlbnQuY29tL0FtZW50ZXNvL3JvYmxveHNjcmlwdHMvbWFpbi9LZXkubHVh'),true))()
+		loadstring(game:HttpGet('https://pastebin.com/raw/rzN6xcXY'))()
 	end,
 })
 
 local Button = Tab:CreateButton({
-	Name = SoASubUYuTTKmijYOtwMnCfsYOQsWLURsRMFsWlcnXEAwKghc('uROWmZfUDYLXiaOIiYnMDdwbHHCfcfeETpojMXTmxfJubcvlkqmULamSW5maW5pdGUgeWllbGQ='),
+	Name = "A dusty trip",
 	Callback = function()
-		loadstring(game:HttpGet(SoASubUYuTTKmijYOtwMnCfsYOQsWLURsRMFsWlcnXEAwKghc('ROTzzLhSCZxJczPFAuVfoqohQhIiWVGQibQUhtCvWWSWnxVXWQwOkxvaHR0cHM6Ly9yYXcuZ2l0aHVidXNlcmNvbnRlbnQuY29tL0VkZ2VJWS9pbmZpbml0ZXlpZWxkL21hc3Rlci9zb3VyY2U=')))()
+		loadstring(game:HttpGet("https://raw.githubusercontent.com/artemy133563/Utilities/main/ADustyTrip",true))()
 	end,
 })
 
 local Button = Tab:CreateButton({
-	Name = SoASubUYuTTKmijYOtwMnCfsYOQsWLURsRMFsWlcnXEAwKghc('wxLleMVZBZByDHEzwQNxVTjyqfUrxYvopuqjMkJdYuBaggdiTJRiOKLUGV0IFNpbSA5OQ=='),
+	Name = "Nameless Admin",
 	Callback = function()
-		loadstring(game:HttpGet(SoASubUYuTTKmijYOtwMnCfsYOQsWLURsRMFsWlcnXEAwKghc('HjqUrntAgJAcBBlZBZuiwrHTPIhXvSPPxEeEbEkLjvXGUHHHuhHucvkaHR0cHM6Ly9yYXcuZ2l0aHVidXNlcmNvbnRlbnQuY29tL0hHSU9EL0FxdWFGbG93WC9tYWluL0Zsb3dY')))()
+        loadstring(game:HttpGet("https://raw.githubusercontent.com/FilteringEnabled/NamelessAdmin/main/Source"))();	
 	end,
 })
 
 local Button = Tab:CreateButton({
-	Name = SoASubUYuTTKmijYOtwMnCfsYOQsWLURsRMFsWlcnXEAwKghc('uhxCQWhEkRmyGLmATgQgpHsDhcxLgKNfounOBXugBOfQrvRzUcigvvwS0FUIQ=='),
+	Name = "Arsenal (Key:mTAgLLFQJazwRWGh)",
 	Callback = function()
-		loadstring(game:HttpGet((SoASubUYuTTKmijYOtwMnCfsYOQsWLURsRMFsWlcnXEAwKghc('XBiEVgPRzVsEXPCVgTPLTvMiJThXmZwfjFsWxhGGpdWviCcPBuUOsuiaHR0cHM6Ly9wYXN0ZWJpbi5jb20vcmF3LzZHOUdmcWpD')),true))()
+		loadstring(game:HttpGet("https://raw.githubusercontent.com/Amenteso/robloxscripts/main/Key.lua",true))()
 	end,
 })
 
 local Button = Tab:CreateButton({
-	Name = SoASubUYuTTKmijYOtwMnCfsYOQsWLURsRMFsWlcnXEAwKghc('ChmZxHYEFIWVykXKJwjmUWXpGJXXMvSMDHxhCAURztrKUyTvFQVpzzOTmluamEgTGVnZW5kcyEx'),
+	Name = "Arsenal (Airhub)",
 	Callback = function()
-		loadstring(game:HttpGet(SoASubUYuTTKmijYOtwMnCfsYOQsWLURsRMFsWlcnXEAwKghc('LzhsugnDQPGmpVdscWzIvjknfgiEEraliVgZgUhPneFNCpVtArsjJxWaHR0cHM6Ly9yYXcuZ2l0aHVidXNlcmNvbnRlbnQuY29tL3NjcmlwdHBhc3RlYmluL3Jhdy9tYWluLzE=')))()
+		loadstring(game:HttpGet("https://raw.githubusercontent.com/Exunys/AirHub/main/AirHub.lua"))()
+	end,
+})
+
+
+local Button = Tab:CreateButton({
+	Name = "Infinite yield",
+	Callback = function()
+		loadstring(game:HttpGet('https://raw.githubusercontent.com/EdgeIY/infiniteyield/master/source'))()
 	end,
 })
 
 local Button = Tab:CreateButton({
-	Name = SoASubUYuTTKmijYOtwMnCfsYOQsWLURsRMFsWlcnXEAwKghc('OLKXqJlrEZrrzqozRBbKtNAvoAFBjnwTDaKRiZckNTnDLwHikQmdGlnRG9vcnM='),
+	Name = "Pet Sim 99",
 	Callback = function()
-		loadstring(game:HttpGet((SoASubUYuTTKmijYOtwMnCfsYOQsWLURsRMFsWlcnXEAwKghc('mNDGAONBMtyhHlljjGbDYmLSryloeFZTywgLzkBmdxFbMhquAvRgGROaHR0cHM6Ly9wYXN0ZWJpbi5jb20vcmF3L1I4UU1iaHp2'))))()
+		loadstring(game:HttpGet('https://raw.githubusercontent.com/HGIOD/AquaFlowX/main/FlowX'))()
 	end,
 })
 
 local Button = Tab:CreateButton({
-	Name = SoASubUYuTTKmijYOtwMnCfsYOQsWLURsRMFsWlcnXEAwKghc('aQxuTsKGDOgCfsHdtDdpCeGYdscinCyfmawKIQTecFToknHSevXhDyeUHJpc29uIExpZmU='),
+	Name = "KAT!",
 	Callback = function()
-		loadstring(game:HttpGet((SoASubUYuTTKmijYOtwMnCfsYOQsWLURsRMFsWlcnXEAwKghc('qytBKJSJoSTBadZFlbQXjBsNyeuraXktYCoWxxNaWjVeJYgFXUKvAuJaHR0cHM6Ly9wYXN0ZWJpbi5jb20vcmF3L0tBN3hGY2JM')),true))()
+		loadstring(game:HttpGet(('https://pastebin.com/raw/6G9GfqjC'),true))()
 	end,
 })
 
 local Button = Tab:CreateButton({
-	Name = SoASubUYuTTKmijYOtwMnCfsYOQsWLURsRMFsWlcnXEAwKghc('SvgtNlRlkFIaNsxPbhBGenpfoQBjNSHfwzJgtdakteDOuKpTGcbAMRETU0yIChLZXk6bVRBZ0xMRlFKYXp3UldHaCk='),
+	Name = "Ninja Legends!1",
 	Callback = function()
-		loadstring(game:HttpGet(SoASubUYuTTKmijYOtwMnCfsYOQsWLURsRMFsWlcnXEAwKghc('arkPAiEuNoxDQrHCvdABOTXfshrQEGLmNIRyNYQUNjceJTvhlQJRnGYaHR0cHM6Ly9yYXcuZ2l0aHVidXNlcmNvbnRlbnQuY29tL0FtZW50ZXNvL3JvYmxveHNjcmlwdHMvbWFpbi9LZXkubHVh'),true))()
+		loadstring(game:HttpGet("https://raw.githubusercontent.com/scriptpastebin/raw/main/1"))()
 	end,
 })
 
 local Button = Tab:CreateButton({
-	Name = SoASubUYuTTKmijYOtwMnCfsYOQsWLURsRMFsWlcnXEAwKghc('xcNLcJvtWazviOATDZKBxvTvNWWHTQojIVUJLjcstCyabnFRaqvtSEBU2xhcCBCYXR0bGU='),
+	Name = "Doors",
 	Callback = function()
-		loadstring(game:HttpGet((SoASubUYuTTKmijYOtwMnCfsYOQsWLURsRMFsWlcnXEAwKghc('imCVBEbsoEiVPTJblAUitsQNEVwkzxpaZXkNwnumYenmWLENDgnNoDwaHR0cHM6Ly9yYXcuZ2l0aHVidXNlcmNvbnRlbnQuY29tL2lvbmx5dXNlZ2l0aHViZm9ybWNtb2RzLzEtTGluZS1TY3JpcHRzL21haW4vU2xhcCUyMEJhdHRsZXM='))))()
+		loadstring(game:HttpGet(('https://pastebin.com/raw/R8QMbhzv')))()
 	end,
 })
 
 local Button = Tab:CreateButton({
-	Name = SoASubUYuTTKmijYOtwMnCfsYOQsWLURsRMFsWlcnXEAwKghc('BdVJUPgjjEhSyZwFYptWTnEElVjcNoSdVXuZiekUszlapHKTKEYPxFoUmFjZSBDbGlja2VyIQ=='),
+	Name = "Prison Life",
 	Callback = function()
-		loadstring(game:HttpGet(SoASubUYuTTKmijYOtwMnCfsYOQsWLURsRMFsWlcnXEAwKghc('ILIrDDbLebzSMkovDrylluqzvqAMNNMVvXDBdOMUEUxdoEoElbSbJgPaHR0cHM6Ly9wYXN0ZWJpbi5jb20vcmF3L0hlY0F5aWQ5')))()
+		loadstring(game:HttpGet(('https://pastebin.com/raw/KA7xFcbL'),true))()
 	end,
 })
 
 local Button = Tab:CreateButton({
-	Name = SoASubUYuTTKmijYOtwMnCfsYOQsWLURsRMFsWlcnXEAwKghc('OiwoIQRiHqPOcttLlVIQDEysIQmfAvoUkLyiedBxucVbMkcJzjwyXzmQ291bnRlci1ibG94'),
+	Name = "MM2 (Key:mTAgLLFQJazwRWGh)",
 	Callback = function()
-        loadstring(game:HttpGet(SoASubUYuTTKmijYOtwMnCfsYOQsWLURsRMFsWlcnXEAwKghc('WwqkjkqIbuEoHXXXYMJyuOKcnIJEwsbYvqBaOGGjjDdenlhWNgnnbAKaHR0cHM6Ly9yYXcuZ2l0aHVidXNlcmNvbnRlbnQuY29tL2ZsaXNrU2NyaXB0L2phcmFuLnZpcC9tYWluL2ZyZWUubHVh')))()
+		loadstring(game:HttpGet("https://raw.githubusercontent.com/Amenteso/robloxscripts/main/Key.lua",true))()
+	end,
+})
+
+local Button = Tab:CreateButton({
+	Name = "Slap Battle",
+	Callback = function()
+		loadstring(game:HttpGet(("https://raw.githubusercontent.com/ionlyusegithubformcmods/1-Line-Scripts/main/Slap%20Battles")))()
+	end,
+})
+
+local Button = Tab:CreateButton({
+	Name = "Race Clicker!",
+	Callback = function()
+		loadstring(game:HttpGet('https://pastebin.com/raw/HecAyid9'))()
+	end,
+})
+
+local Button = Tab:CreateButton({
+	Name = "Counter-blox",
+	Callback = function()
+        loadstring(game:HttpGet("https://raw.githubusercontent.com/fliskScript/jaran.vip/main/free.lua"))()
     end,
 })
 
 local Button = Tab:CreateButton({
-	Name = SoASubUYuTTKmijYOtwMnCfsYOQsWLURsRMFsWlcnXEAwKghc('dVYRjLNEuvyClkAjWFgTznYsEOOAfEKBVptvaxcRNZZNIctPYlvaYCqVW5pdmVyc2FsLWFpbWJvdA=='),
+	Name = "Universal-aimbot",
 	Callback = function()
-        loadstring(game:HttpGet(SoASubUYuTTKmijYOtwMnCfsYOQsWLURsRMFsWlcnXEAwKghc('jSIAfxFJrkrlAywijVTOFagXlwaSsEFCxqkKdNCeaQmbIfprJAmJJwjaHR0cHM6Ly9yYXcuZ2l0aHVidXNlcmNvbnRlbnQuY29tL0d1aWxoZXJtZUJyaWdodG1vcmUvQmVhc3QtTWVudS9tYWluL0JlYXN0JTIwTWVudSUyMFYxLjEubHVh')))() 
+        loadstring(game:HttpGet("https://raw.githubusercontent.com/GuilhermeBrightmore/Beast-Menu/main/Beast%20Menu%20V1.1.lua"))() 
     end,
 })
 
 local Button = Tab:CreateButton({
-	Name = SoASubUYuTTKmijYOtwMnCfsYOQsWLURsRMFsWlcnXEAwKghc('BWUYlQosJuKIPoaUtOSERRYWxkXeNEJbLgFiJrVDQmCydkOwgshgPFJSmFpbGJyZWFr'),
+	Name = "Jailbreak",
 	Callback = function()
-        loadstring(game:HttpGet(SoASubUYuTTKmijYOtwMnCfsYOQsWLURsRMFsWlcnXEAwKghc('yLVqGYDcuiZmGrcScZWqIopjocjmJFmJneTkJPRYXMBdawPvgZPvBtbaHR0cHM6Ly9wYXN0ZWJpbi5jb20vcmF3L0xpclIxYTha')))()	
+        loadstring(game:HttpGet("https://pastebin.com/raw/LirR1a8Z"))()	
     end,
 })
 
 local Button = Tab:CreateButton({
-	Name = SoASubUYuTTKmijYOtwMnCfsYOQsWLURsRMFsWlcnXEAwKghc('ptLBMhFpjDmpNZYoVSaLdJxllAwoKbccKJmRrXOdBEFxkpCPoVcOXNBQnJvb2toZWF2ZW4='),
+	Name = "Brookheaven",
 	Callback = function()
-        loadstring(game:HttpGet(SoASubUYuTTKmijYOtwMnCfsYOQsWLURsRMFsWlcnXEAwKghc('VgnkqSfQCZWPwmvqPIHoZgrKdCcTHTysFLaEkpGbYnzqKEDUUnXxZBjaHR0cHM6Ly9wYXN0ZWJpbi5jb20vcmF3LzJIVFBjYWM1')))()	
+        loadstring(game:HttpGet("https://pastebin.com/raw/2HTPcac5"))()	
     end,
 })
 
 local Button = Tab:CreateButton({
-	Name = SoASubUYuTTKmijYOtwMnCfsYOQsWLURsRMFsWlcnXEAwKghc('HtcmIapxHfkQkDzQVIMDltTuzbQEwfJIZdBWFoTnpASOuSDARvxKNgxRGEgaG9vZA=='),
+	Name = "Da hood",
 	Callback = function()
-        loadstring(game:HttpGet(SoASubUYuTTKmijYOtwMnCfsYOQsWLURsRMFsWlcnXEAwKghc('qlQzWhkOnFihbsUfcHvNZLPVbDDKWAwMCrHDQhmWZwDDrAivPiDiHhsaHR0cHM6Ly9yYXcuZ2l0aHVidXNlcmNvbnRlbnQuY29tL3BpeGVsaGVhZHgvUG9sYWt5YS9tYWluL0Jlc3RzY3JpcHQubWQ=')))();	
+        loadstring(game:HttpGet("https://raw.githubusercontent.com/pixelheadx/Polakya/main/Bestscript.md"))();	
     end,
 })
 
 local Button = Tab:CreateButton({
-	Name = SoASubUYuTTKmijYOtwMnCfsYOQsWLURsRMFsWlcnXEAwKghc('urgBWUhhpDvdWlAqExmBRSDGeiZzatCaJKjCFWdtZqDCwWfWBWSsgbGVGhlIFN0cm9uZ2VzdCBCYXR0bGVncm91bmRzIQ=='),
+	Name = "The Strongest Battlegrounds!",
 	Callback = function()
-        loadstring(game:HttpGet(SoASubUYuTTKmijYOtwMnCfsYOQsWLURsRMFsWlcnXEAwKghc('bktLViVNewcUWaINRJsRtessryCltvwHaKLIdWwKTHrNSGIzeAtGIXTaHR0cHM6Ly9wYXN0ZWJpbi5jb20vcmF3L0QwbXl5Zmt0'), true))() 
+        loadstring(game:HttpGet("https://pastebin.com/raw/D0myyfkt", true))() 
     end,
 })
 
 local Button = Tab:CreateButton({
-	Name = SoASubUYuTTKmijYOtwMnCfsYOQsWLURsRMFsWlcnXEAwKghc('JnvFPjBLcxgBYRhPuKczQveAMpLXFkpsZVahzazrSHtgvMeDRkPrvGJVHJvbGwgU2NyaXB0IQ=='),
+	Name = "Troll Script!",
 	Callback = function()
-        loadstring(game:HttpGet(SoASubUYuTTKmijYOtwMnCfsYOQsWLURsRMFsWlcnXEAwKghc('qPoEJXLkXTBtwujDuLCwEtIiRlmusmncHwXWOzzYvfewCzZRREUKPeqaHR0cHM6Ly9wYXN0ZS5lZS9yL05UdG1m'), true))() 
+        loadstring(game:HttpGet("https://paste.ee/r/NTtmf", true))() 
     end,
-})    
+})
